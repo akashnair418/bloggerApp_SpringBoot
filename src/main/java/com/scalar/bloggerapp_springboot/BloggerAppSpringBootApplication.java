ffe4050e8cfe6ac098efabb98db1469e8ba89eb6
Acer <nairakash418@gmail.com>
2025-04-05 23:15:46 +0530

    Initial commit

---
+++ src/main/java/com/scalar/bloggerapp_springboot/BloggerAppSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.scalar.bloggerapp_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BloggerAppSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BloggerAppSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/scalar/bloggerapp_springboot/BloggerAppSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scalar.bloggerapp_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BloggerAppSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
